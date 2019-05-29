e574360bebe80d19df0088816ccf03f42fde734d
Artemy22 <artem.nii@me.com>
2019-05-29 10:47:02 +0300

    Lesson 19 fix (#7)
    * lesson-17
    
    * lesson-17
    
    * lesson-19
    
    * lesson-19#2
    
    * lesson-19-fix

--- lesson-19-symphony/src/Controller/PhoneController.php
+++ lesson-19-symphony/src/Controller/PhoneController.php
@@ -2,36 +2,35 @@
 
 namespace App\Controller;
 
-use App\Entity\Country;
 use App\Entity\Phone;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CountryController extends AbstractController
+
+class PhoneController extends AbstractController
 {
     /**
      * @var UserRepository
      */
     private $repositoryUser;
-
     public function __construct()
     {
         //$this->repositoryUser = $this->getDoctrine()->getRepository(User::class);
     }
 
     /**
-     * @Route("/countries", name="country")
+     * @Route("/phones", name="phone")
      */
     public function index()
     {
-        $repositoryUser = $this->getDoctrine()->getRepository(Country::class);
+        $repositoryUser = $this->getDoctrine()->getRepository(Phone::class);
 
-        $countries = $repositoryUser->findAll();
+        $phones = $repositoryUser->findAll();
 
-        return $this->render('country/index.html.twig', [
-            'countries' => $countries,
+        return $this->render('phone/index.html.twig', [
+            'phones' => $phones,
         ]);
     }
-
+}
